89e7474f7cf19c14dd03a9d1b6b4f1f21c788dd0
GitThisJoel <joelbacker10@gmail.com>
2022-01-21 16:40:31 +0100

    assignment 1 started, ce2 and e3

---
+++ 1/ce2_joel.m
@@ -0,0 +1,14 @@
+load("assignment1data/compEx2.mat");
+
+im = imread("assignment1data/compEx2.jpg");
+
+figure()
+hold on
+
+imshow(im);
+colormap(gray);
+
+% rital(x2D, '-');
+
+axis equal
+hold off
