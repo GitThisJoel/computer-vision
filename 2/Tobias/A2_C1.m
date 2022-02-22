30cfe2dda1491a04936395cf0807e38ae2902d1c
TobiasBjork <to8387bj-s@student.lu.se>
2022-02-22 17:07:19 +0100

    small fix

--- 2/Tobias/A2_C1.m
+++ 2/Tobias/A2_C1.m
@@ -6,7 +6,7 @@ load("compEx1data.mat")
 
 im = imread(imfiles{1});
 
-figure(1)
+figure()
 hold on
 plot3(X(1,:),X(2,:),X(3,:),'b.','Markersize',2); %plots the 3D points of reconstruction
 plotcams(P) %plot the cameras
@@ -18,13 +18,13 @@ imageplot(1,imfiles,X,P,x);
 T1 = [1 0 0 0;0 4 0 0; 0 0 1 0; 0.1 0.1 0 1];
 T2 = [1 0 0 0 ; 0 1 0 0; 0 0 1 0; 1/16 1/16 0 1];
 
-X1 = pflat(T1\X); %new projective solution
-X2 = pflat(T2\X);
+X1 = pflat(T1*X); %new projective solution
+X2 = pflat(T2*X);
 P1 = P;
 P2 = P;
 for i=1:9
-P1{i} = P{i}*T1; % new cameras
-P2{i} = P{i}*T2;
+P1{i} = P{i}*inv(T1); % new cameras
+P2{i} = P{i}*inv(T2);
 end
 %Plots of new solutions
 figure()
@@ -32,11 +32,12 @@ hold on
 plot3(X1(1,:),X1(2,:),X1(3,:),'b.','Markersize',2);
 plotcams(P1);
 axis equal
+% 
+% figure()
+% hold on
+% plot3(X2(1,:),X2(2,:),X2(3,:),'b.','Markersize',2);
+% plotcams(P2);
 
-figure()
-hold on
-plot3(X2(1,:),X2(2,:),X2(3,:),'b.','Markersize',2);
-plotcams(P2);
 axis equal
 
 %projecting and plotting the points in the images
