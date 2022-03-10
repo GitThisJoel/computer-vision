d07a5a16b0f25ab913cdfc59f4d780296a8a2b4e
GitThisJoel <joelbacker10@gmail.com>
2022-03-10 23:57:06 +0100

    done with ce3

---
+++ 4/joel/ce3.m
@@ -0,0 +1,39 @@
+clear
+close all
+
+addpath('../assignment4data')
+load('ce2_variables.mat')
+
+gammak = 10^-10;
+
+% Takes two camera matrices and puts them in a cell.
+P = {P1,P2};
+
+U = X;
+u = x;
+
+% Computes the reprejection error and the values of all the residuals
+% for the current solution P,U,u.
+[err,res] = ComputeReprojectionError(P,U,u);
+
+Pnew = P;
+Unew = U;
+
+figure();
+plot(0, sum(res), 'r*')
+hold on
+for i=1:10
+    % Computes the r and J matrices for the appoximate linear least squares problem .
+    [r,J] = LinearizeReprojErr(Pnew,Unew,u);
+    
+    % Computes the LM update.
+    deltav = -gammak*J'*r;
+    
+    % Updates the variabels
+    [Pnew, Unew] = update_solution(deltav,Pnew,Unew);
+    
+    [err,res] = ComputeReprojectionError(Pnew,Unew,u);
+    
+    plot(i, sum(res), 'r*')
+end 
+RMS = sqrt(err/size(res,2))
