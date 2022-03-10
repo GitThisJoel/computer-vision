8031470534ec65975829bcdc01dff24b1d511c85
GitThisJoel <joelbacker10@gmail.com>
2022-03-10 13:34:58 +0100

    done with computer exercise 1

--- 4/joel/ce1.m
+++ 4/joel/ce1.m
@@ -1,3 +1,6 @@
+clear
+close all
+
 addpath('../assignment4data')
 addpath("../../vlfeat-0.9.21/toolbox");
 vl_setup;
@@ -5,36 +8,68 @@ vl_setup;
 A = imread('a.jpg');
 B = imread('b.jpg');
 
-
-[fA dA] = vl_sift(single(rgb2gray(A)));%, 'PeakThresh' , 1);
-[fB dB] = vl_sift(single(rgb2gray(B)));%, 'PeakThresh' , 1);
+[fA dA] = vl_sift(single(rgb2gray(A)));
+[fB dB] = vl_sift(single(rgb2gray(B)));
 % vl_plotframe(f1);
 [matches, scores] = vl_ubcmatch(dA, dB);
 
-xA = fA(1:2, matches(1,:));
-xB = fB(1:2, matches(2,:));
+xA = [fA(1:2, matches(1,:)); ones(1, size(matches,2))];
+xB = [fB(1:2, matches(2,:)); ones(1, size(matches,2))];
 
-for j=1:size(x1_norm, 2)
-    M = [P1 -x1_norm(:,j) zeros(3,1);
-        P2 zeros(3,1) -x2_norm(:,j)];
+best_H = [];
+most_inls = 0;
+min_corr = 4;
+for i=1:30
+    r = randperm(size(xA, 2), min_corr);
+    rxA = xA(:,r);
+    rxB = xB(:,r);
+    M = zeros(3*size(rxA,2), 3*size(rxA,1)+min_corr);
+    
+    row = 1;
+    col = 3*size(rxA,1)+1;
+    for j=1:size(rxA, 2)
+        n = size(rxA,1);
+        m = [rxA(:,j)'    zeros(1,2*n); 
+             zeros(1,n)  rxA(:,j)' zeros(1,n); 
+             zeros(1, 2*n)        rxA(:,j)'];  
+         M(row:row+2, 1:9) = m;
+         M(row:row+2, col) = -rxB(:,j);
+         
+         row = row + 3;
+         col = col + 1;
+    end
+    
     [U, S, V] = svd(M);
     v = V(:, end);
-    xs(:,j) = v(1:4);
+    H = [v(1:3)'; v(4:6)'; v(7:9)'];
+    
+    txA = zeros(size(xA));
+    for j=1:size(xA,2)
+        txA(:,j) = pflat(H*xA(:,j));
+    end 
+    
+    inls = sqrt(sum((txA(1:2,:) - xB(1:2,:)).^2))<5;
+    nbr_inls = sum(inls(:) == 1);
+    if nbr_inls > most_inls
+        best_H = H;
+        most_inls = nbr_inls;
+    end 
+end
 
+Htform = projective2d(best_H');
 
-% Htform = projective2d(bestH');
-% 
-% Rout = imref2d(size(A), [-200 800], [-400 600]);
-% 
-% [Atransf] = imwarp(A, Htform, 'OutputView', Rout);
-% 
-% Idtform = projective2d (eye(3));
-% [Btransf] = imwarp(B, Idtform, 'OutputView', Rout);
-% 
-% AB = Btransf;
-% AB(Btransf < Atransf) = Atransf(Btransf < Atransf);
-% 
-% imagesc(Rout.XWorldLimits, Rout.YWorldLimits ,AB);
+% Rout = imref2d(size(A), [-200 1600], [-400 1800]);
+Rout = imref2d(size(A), [-200 800], [-400 600]);
+
+[Atransf] = imwarp(A, Htform, 'OutputView', Rout);
+
+Idtform = projective2d (eye(3));
+[Btransf] = imwarp(B, Idtform, 'OutputView', Rout);
+
+AB = Btransf;
+AB(Btransf < Atransf) = Atransf(Btransf < Atransf);
+
+imagesc(Rout.XWorldLimits, Rout.YWorldLimits ,AB);
 
 
 
